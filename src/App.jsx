a8b7860070f2dd9b17521324d4197c5fb571e817
Adri-shikhar <adrishikharbarua77452@gmail.com>
2026-04-01 00:33:44 +0600

    fetch the  api data at the modal

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import Steps from './components/Steps'
 import Pricing from './components/Pricing'
 import Banner from './components/Banner'
 import Footer from './components/Footer'
+import Modal from './components/modal'
+ const getData = async () => {
+  const response = await fetch("/digitools-platform/data.json");
+  return response.json();
+};
+
+const dataPromise=getData();
+
+ 
+
 
 function App() {
   return (
     <>
       <Nav />
       <Hero />
+      <Modal dataPromise={dataPromise}/>
       <Steps />
       <Pricing />
       <Banner />
---
+++ src/components/Modal.jsx
@@ -0,0 +1,21 @@
+import React, { use } from 'react';
+
+const Modal = ({dataPromise}) => {
+    const data=use(dataPromise);
+    console.log(data);
+    return (
+        <div className='bg-white text-black'>
+            <h1>Modal Title</h1>
+            <div>
+            {data.map((item)=>(
+                <div key={item.id}>
+                    <h2>{item.name}</h2>
+                    <p>{item.price}</p>
+                </div>
+            ))}
+        </div>
+        </div>
+    );
+};
+
+export default Modal;
