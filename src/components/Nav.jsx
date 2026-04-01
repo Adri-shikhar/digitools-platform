e5562351ff6bde41895eb2ddd59766e2b2d68610
Adri-shikhar <adrishikharbarua77452@gmail.com>
2026-04-01 07:58:55 +0600

    finished the project and made the readme file

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -22,7 +22,10 @@ export default function Nav({ carts }) {
                             </svg>
                         </button>
                        
-                            
+                            <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+                                {carts.length}
+                            </span>
+                       
                     </div>
 
 
